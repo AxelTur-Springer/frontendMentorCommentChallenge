f84c77b19c6f2f255d8d73f60f46354819ad8794
AxelTur-Springer <axel.turspringer@gmail.com>
2022-03-23 20:55:55 -0300

    just adding some new functionality / made input recieve value and store in a variable then on submit delete comment form and post comment

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-document.querySelectorAll('.replyBtn').forEach(item => {
-    item.addEventListener('click', event => {
 
-     
-    })
-  })
+  // Function that creates a form to respond a comment and gives it the general style
+ 
   function idOfResponse(id){
-    console.log(id)
+
     let newDiv = document.createElement("div")
     let apendTo = document.getElementById(id);
      newDiv.innerHTML = document.getElementById("CopyCommentBox").innerHTML
      newDiv.classList.add('CopyCommentBox');
+     newDiv.style.display="flex"
     apendTo.appendChild(newDiv)
+
     }
+
+// Function to control comment delete
+
+    function deleteComm(id){
+let toDelete =document.getElementById(id)
+toDelete.remove()
+}
+
+function retrieveComment(Comment,commentBoxRemove,appendId){
+    commentBoxRemove.remove();
+    
+    let newDiv = document.createElement("div");
+    document.getElementById(appendId);
+    let apendTo = document.getElementById(appendId);
+     newDiv.innerHTML = document.getElementById("me").innerHTML;
+   
+     
+     newDiv.classList.add('CopyCommentBox');
+     newDiv.style.display="flex";
+     newDiv.lastElementChild.lastElementChild.lastElementChild.innerHTML =Comment;
+     apendTo.appendChild(newDiv)
+
+     console.log(newDiv.lastElementChild.lastElementChild.lastElementChild)
+}
+
